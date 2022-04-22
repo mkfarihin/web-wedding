43a87197e0a7ea1dcb0a351897c4800ca2478c5f
mkfarihin <farikholik@gmail.com>
2022-04-22 03:33:57 -0700

    first commit

---
+++ app/Views/layout/menu.php
@@ -0,0 +1,17 @@
+<li class="menu-header">Main Menu</li>
+<li><a class="nav-link" href=""><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+<li><a class="nav-link" href=""><i class="far fa-calendar"></i> <span>Acara</span></a></li>
+<li class="nav-item dropdown">
+    <a href="#" class="nav-link has-dropdown"><i class="far fa-addres-book"></i><span>Kontak</span></a>
+    <ul class="dropdown-menu">
+        <li><a class="nav-link" href="">Grup Kontak</a></li>
+        <li><a class="nav-link" href="">Kontak Saya</a></li>
+    </ul>
+</li>
+<li class="nav-item dropdown">
+    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="far fa-envelope"></i> <span>Undangan</span></a>
+    <ul class="dropdown-menu">
+        <li><a class="nav-link" href="">Saya Mengundang</a></li>
+        <li><a class="nav-link" href="">Saya Diundang</a></li>
+    </ul>
+</li>
